8c24491e2fddf8a35177240ba9db34ca77f3ce4a
iason <iason.pifeas@gmail.com>
2025-07-10 14:30:49 +0200

    10.07

--- src/main/java/org/pets/Main.java
+++ src/main/java/org/pets/Main.java
@@ -10,12 +10,12 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
-        Household household = new Household(1, "Adresse1");
+        Household household = new Household(1, "Adresse2");
         System.out.println(household);
 
         HouseholdDAO petDAO = new HouseholdDAOImpl();
 
-        int result = 0;
+        int result;
         try {
             result = petDAO.insert(household);
         } catch (SQLException e) {
