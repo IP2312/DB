0a8a4f15af259fcf824c7d5dd27f59cf64a9270d
iason <iason.pifeas@gmail.com>
2025-07-21 15:29:56 +0200

    21.07

--- src/main/java/org/pets/dao/PetDAOImpl.java
+++ src/main/java/org/pets/dao/PetDAOImpl.java
@@ -1,4 +1,34 @@
 package org.pets.dao;
 
-public class PetDAOImpl {
+import org.pets.model.Pet;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class PetDAOImpl implements PetDAO{
+    @Override
+    public Pet get(int id) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public ArrayList<Pet> getAll() throws SQLException {
+        return null;
+    }
+
+    @Override
+    public int insert(Pet pet) throws SQLException {
+        String s
+        return 0;
+    }
+
+    @Override
+    public int update(Pet pet) throws SQLException {
+        return 0;
+    }
+
+    @Override
+    public int delete(int id) throws SQLException {
+        return 0;
+    }
 }
